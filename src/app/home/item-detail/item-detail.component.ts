11b0ba513a53a864fa5c28940a3f9f85f141fed9
long <thetachyon107@gmail.com>
2020-03-22 22:18:04 +0700

    display product

--- src/app/home/item-detail/item-detail.component.ts
+++ src/app/home/item-detail/item-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Items} from '../item/item.model';
 import {ItemService} from '../item/item.service';
 import {ActivatedRoute, Params} from '@angular/router';
@@ -12,17 +12,25 @@ import {NgForm} from '@angular/forms';
 export class ItemDetailComponent implements OnInit {
   id: number;
   inItems: Items;
+  items: Items[];
   today: number = Date.now();
-  constructor(private activeRoute: ActivatedRoute, private itemServ: ItemService) { }
+
+  constructor(private activeRoute: ActivatedRoute, private itemServ: ItemService) {
+  }
 
   ngOnInit(): void {
+    this.itemServ.cast.subscribe(items => {
+      this.items = items;
+    });
     this.activeRoute.params.subscribe(
       (params: Params) => {
         this.id = params.id;
         this.inItems = this.itemServ.getItemsById(this.id);
+        console.log(this.inItems);
       }
     );
   }
+
   onSubmitRate(form: NgForm) {
     const newrate = parseInt(form.value.rate, 10);
     this.itemServ.addRating(this.id, newrate);
---
+++ src/app/home/item/iitems.ts
@@ -0,0 +1,5 @@
+export interface Iitemss {
+  name: string;
+  price: number;
+  category_id?: number;
+ }
