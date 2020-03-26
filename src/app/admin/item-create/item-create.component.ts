7e927ac35deab37875f8e97c648b2a569af6c430
long <thetachyon107@gmail.com>
2020-03-27 01:29:08 +0700

    update asyn

--- src/app/admin/item-create/item-create.component.ts
+++ src/app/admin/item-create/item-create.component.ts
@@ -30,18 +30,34 @@ export class ItemCreateComponent implements OnInit {
 
   add(data) {
     if (data.name && data.price && data.categories) {
-      const product = {
+      const product1 = {
         name: data.name,
         price: data.price,
         categories: data.categories
       };
-      this.itemService.addItem(product).subscribe(next => {
+      this.itemService.addItem(product1).subscribe(next => {
         this.itemService.getItems().subscribe(next1 => {
-            const products = [];
-            for (const product1 of next1) {
-              products.push(product1[0]);
+            const itemss = [];
+            for (const product of next1) {
+              itemss.push(product[0]);
             }
-            this.itemService.updateItems(products);
+            for (const item of itemss) {
+              item.description = 'haha';
+              item.imagePath = 'https://5.imimg.com/data5/EH/IU/MY-13191810/moto-g5-plus-500x500.png';
+              item.reviewCounter = 3;
+              item.reviews = ['good', 'bad', 'ok'];
+              item.ratingCounter = 3;
+              item.ratings = [3, 5, 3];
+              item.avg = 3.3;
+              item.extraImages = [
+                'https://fscl01.fonpit.de/userfiles/6727621/image/2017/lenovo-moto-g5/AndroidPIT-lenovo-moto-g5-1120-w810h462.jpg',
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQs5vZtDVm21sb-dInaZ-7qJXJCqNKzDTc50UO3-G97A6zGNf5k',
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcREu8RL-q4hMdHZOu9rSRHLxFCVuRT7yvOHJrKlFmrReO0iWqX0',
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTOU0ZGLS85pB9_-b692rHeuY1siTxlalWlaybOuNJhbZJMlKNQ',
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTR1g_Ee8u3W_6o2oBZipC3L6qsDBb5jD9Dde9qMA1BNDXbFzgL'
+              ];
+            }
+            this.itemService.updateItems(itemss);
           }
         );
         this.router.navigate(['home/admin']);
@@ -58,9 +74,11 @@ export class ItemCreateComponent implements OnInit {
   get price() {
     return this.addForm.get('price');
   }
+
   cancel() {
     this.router.navigate(['home/admin']);
   }
+
   onCheckChange(event) {
     const formArray: FormArray = this.addForm.get('categories') as FormArray;
     if (event.target.checked) {
