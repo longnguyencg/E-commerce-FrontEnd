dbc49490ef238c94c671ad1d122c57b277090f5e
Hai-TranC1019i1 <tranhaicoder@gmail.com>
2020-03-30 23:17:25 +0700

    complete cart

--- src/app/home/cart/cart.component.ts
+++ src/app/home/cart/cart.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ICartItem} from './ICartItem';
 import {ItemService} from '../item/item.service';
 import {ActivatedRoute} from '@angular/router';
+import {CartService} from './cart.service';
 
 @Component({
   selector: 'app-category',
@@ -9,79 +10,69 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  private items: ICartItem[];
-  private total: number;
-  private totalTypeProduct: number;
-  private coupons: string[];
-  private shipping: number;
+  public items: ICartItem[];
+  public total: number;
+  public totalTypeProduct: number;
+  public coupons: string[];
+  public shipping: number;
+
   constructor(
     private itemService: ItemService,
     private activatedRoute: ActivatedRoute,
-  ) { }
+    private cartService: CartService,
+  ) {
+  }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params => {
-      const id = params.id;
-      if (id) {
-        const item: ICartItem = {
-          product: this.itemService.getItemsById(id),
-          quantity: 1
-        };
-        if (localStorage.getItem('cart') == null) {
-          const cart: any = [];
-          cart.push(JSON.stringify(item));
-          localStorage.setItem('cart', JSON.stringify(cart));
-        } else {
-          const cart: any = JSON.parse(localStorage.getItem('cart'));
-          let index = -1;
-          for (let i = 0; i < cart.length; i++) {
-            const item: ICartItem = JSON.parse(cart[i]);
-            if (item.product.id == id) {
-              index = i;
-              break;
-            }
-          }
-          if (index == -1) {
-            cart.push(JSON.stringify(item));
-            localStorage.setItem('cart', JSON.stringify(cart));
-          } else {
-            const item: ICartItem = JSON.parse(cart[index]);
-            item.quantity += 1;
-            cart[index] = JSON.stringify(item);
-            localStorage.setItem('cart', JSON.stringify(cart));
-          }
-        }
-        this.loadCart();
-      } else {
-        this.loadCart();
-      }
-    });
+    this.loadCart();
+    this.totalTypeProduct = this.items.length;
   }
 
   loadCart(): void {
     this.total = 0;
     this.items = [];
-    const cart = JSON.parse(localStorage.getItem('cart'));
+    let cart = JSON.parse(localStorage.getItem('cart'));
+    // tslint:disable-next-line:prefer-for-of
     for (let i = 0; i < cart.length; i++) {
-      const item = JSON.parse(cart[i]);
+      let item = JSON.parse(cart[i]);
       this.items.push({
         product: item.product,
         quantity: item.quantity
       });
       this.total += item.product.price * item.quantity;
     }
+    this.totalTypeProduct = cart.length;
+    this.cartService.typeOfProduct = cart.length;
   }
 
-  remove(id: string): void {
-    const cart: any = JSON.parse(localStorage.getItem('cart'));
-    const index = -1;
+  remove(id: number): void {
+    let cart: any = JSON.parse(localStorage.getItem('cart'));
+    let index: number = -1;
     for (let i = 0; i < cart.length; i++) {
-      const item: Item = JSON.parse(cart[i]);
+      let item: ICartItem = JSON.parse(cart[i]);
       if (item.product.id == id) {
         cart.splice(i, 1);
         break;
       }
     }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    this.loadCart();
+  }
+
+  update(id: number, value: number) {
+    if (value < 1) { return; }
+    let cart: any = JSON.parse(localStorage.getItem('cart'));
+    let index = -1;
+    for (let i = 0; i < cart.length; i++) {
+      const item: ICartItem = JSON.parse(cart[i]);
+      if (item.product.id == id) {
+        index = i;
+        break;
+      }
+    }
+    const item: ICartItem = JSON.parse(cart[index]);
+    item.quantity = value;
+    cart[index] = JSON.stringify(item);
     localStorage.setItem('cart', JSON.stringify(cart));
     this.loadCart();
   }
